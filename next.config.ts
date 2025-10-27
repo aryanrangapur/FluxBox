0d8d3c36886f05aa17b9f96dd2326cc9a19a2abd
aryanrangapur <aryan.rangapur717@gmail.com>
2025-10-27 18:07:22 +0000

    Auth Page updated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        'localhost:3000',
+        'zs52xq5p-3000.inc1.devtunnels.ms'
+      ]
+    }
+  }
 };
 
 export default nextConfig;
